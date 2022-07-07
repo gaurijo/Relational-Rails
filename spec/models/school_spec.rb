e8d16e7b00871593c9b88e7723c042f5ca823a90
Gauri Joshi <gauri728@gmail.com>
2022-07-07 11:11:01 -0700

    Refactor model test

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -1,44 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe School, type: :model do
-    describe 'instance methods' do   
-        it 'returns the name of the school' do
-         school = School.create!(name: "Lemonade High School", national_rank: 12, 
-                                ap_program: true)
-         school_2 = School.create!(name: "Watermelon High School", national_rank: 19,
-                                  ap_program: false)
-        
-         expect(school.name).to eq("Lemonade High School")
-         expect(school_2.name).to eq("Watermelon High School")
-        end
-
-        it 'has a national rank' do 
-         school = School.create!(name: "Lemonade High School", national_rank: 12, 
-                                ap_program: true)
-         school_2 = School.create!(name: "Watermelon High School", national_rank: 19,
-                                  ap_program: false)
-        
-        expect(school.national_rank).to eq(12)
-        expect(school_2.national_rank).to eq(19)
-       end
-
-        it 'knows if it has an ap program' do 
-         school = School.create!(name: "Lemonade High School", national_rank: 12, 
-                                ap_program: true)
-         school_2 = School.create!(name: "Watermelon High School", national_rank: 19,
-                                  ap_program: false) 
-
-        expect(school.ap_program?).to eq(true)
-        expect(school_2.ap_program?).to eq(false)
-        end
-
-    end
-
     describe 'validations' do 
         it { should validate_presence_of(:name)}
         it { should validate_presence_of(:national_rank)}
         it { should allow_value(true). for(:ap_program)}
         it { should allow_value(false). for(:ap_program)}
-    end
-                
+    end             
 end
