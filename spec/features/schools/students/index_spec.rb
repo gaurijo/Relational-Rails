3b85fe76a9f14d54f08c91760a81947035a159e7
Gauri Joshi <gauri728@gmail.com>
2022-07-07 16:21:39 -0700

    updates index html and completes user story 5

--- spec/features/schools/students/index_spec.rb
+++ spec/features/schools/students/index_spec.rb
@@ -19,7 +19,27 @@ RSpec.describe 'school students index' do
       expect(page).to have_content(rhonda.name)
    end
 
-   it 'shows all of the student class ranks for the school'
+   it 'shows all of the student honor roll for the school' do 
+      school = School.create!(name: "Lemonade High School", national_rank: 12, 
+                                ap_program: true)
+      mira = school.students.create!(name: "Mira", honor_roll: true, class_rank: 4)
+      rhonda = school.students.create!(name: "Rhonda", honor_roll: false, class_rank: 49)
 
-   it 'shows all of the student honor roll status for the school'
+      visit "/schools/#{school.id}/students"
+
+      expect(page).to have_content(mira.honor_roll)
+      expect(page).to have_content(rhonda.honor_roll)
+   end
+
+   it 'shows all of the student class rank status for the school' do 
+      school = School.create!(name: "Lemonade High School", national_rank: 12, 
+                                ap_program: true)
+      mira = school.students.create!(name: "Mira", honor_roll: true, class_rank: 4)
+      rhonda = school.students.create!(name: "Rhonda", honor_roll: false, class_rank: 49)
+
+      visit "/schools/#{school.id}/students"
+
+      expect(page).to have_content(mira.class_rank)
+      expect(page).to have_content(rhonda.class_rank)
+   end
 end
