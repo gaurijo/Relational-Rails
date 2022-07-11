f67650d5c180b6cadd109530411c645e7a2e9607
Gauri Joshi <gauri728@gmail.com>
2022-07-11 13:21:39 -0700

    adds route to students - id - edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/students', to: 'students#index'
+  get '/students/:id/edit', to: 'students#edit'
   get '/students/:id', to: 'students#show'
   get 'schools/:school_id/students', to: 'school_students#index'
   get 'schools/:school_id/students/new', to: 'school_students#new'
