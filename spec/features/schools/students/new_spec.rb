a4c970d361324f4e90e4a5fad2b9389fa638fbc6
Gauri Joshi <gauri728@gmail.com>
2022-07-10 21:00:31 -0700

    tests population of form in the correct route

--- spec/features/schools/students/new_spec.rb
+++ spec/features/schools/students/new_spec.rb
@@ -19,4 +19,16 @@ RSpec.describe 'the School Student creation' do
    end
 # When I click the link
 # I am taken to '/schools/:school_id/students/new' where I see a form to add a new adoptable child
+
+   it 'populates a form to create a new student for that school' do 
+      school = School.create!(name: "Lemonade High School", national_rank: 12, 
+                                 ap_program: true)
+      student = school.students.create!(name: "Mira", honor_roll: true, class_rank: 4)
+
+      visit "/schools/#{school.id}/students/new"
+
+      click_button('Create Student')
+
+      expect(current_path).to eq("/schools/#{school.id}/students")
+   end
 end
